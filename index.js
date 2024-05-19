2be36790ecb4b6462eb5810307604bafd7f01cd2
sami5671 <samialam5671@gmail.com>
2024-05-19 10:56:12 +0600

    update

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const port = process.env.PORT || 5000;
 
 // -------------------middleware----------------------------------------------------------------
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  // origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: ["https://nsc-encryption-decryption.web.app"],
   credentials: true,
   optionSuccessStatus: 200,
 };
